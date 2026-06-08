f299ef3db5ea87a75c8482092df35d62bfee135b
Juan Peyrot <juanpeyrot15@gmail.com>
2026-06-07 21:37:49 -0300

    init

---
+++ api/cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+
+	"shrt/internal/handlers"
+)
+
+func main() {
+	appEnv := os.Getenv("APP_ENV")
+
+	if appEnv == "" || appEnv == "development" {
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("Error loading .env file:", err)
+		}
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/health", handlers.Health)
+
+	fmt.Println("Server running on: " + port)
+	http.ListenAndServe(":"+port, r)
+}
